3e36c8e4ba6459a8f850aa03e4e8d4f767255ec7
nasrin haghighi <ns.haghighi@gmail.com>
2024-05-16 18:30:14 +0100

    Adding SignbyGoogle page component

---
+++ app/components/SigninbyGoogle/SignbyGoogle.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import React from 'react'
+import { useSession,signIn } from 'next-auth/react'
+
+
+function LoginPage() {
+ 
+ const {data, status} = useSession()
+ console.log(data)
+if(status === 'loading') return <div>Loading...</div>
+if(status === 'authenticated') 
+{
+//router.push('/')
+}
+   
+  return (
+    <div className='container text-center max-w-screen-xl '>
+     
+        <div className='container p-10  rounded-md w-[300px] flex flex-col gap-10  m-auto'>
+          <div className='bg-red p-5 text-textColor rounded-md cursor-pointer '
+           onClick={() => signIn('google')}
+          >Sign in with Google</div>
+        </div>
+    </div>
+
+  )
+}
+
+export default LoginPage
